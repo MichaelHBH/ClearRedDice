4013560712d4532c000b1163ee14654336f9e79f
MichaelHBH <32097294+MichaelHBH@users.noreply.github.com>
2017-09-20 22:01:01 +0900

    Branch to test look up

--- CRD.rb
+++ CRD.rb
@@ -4,10 +4,12 @@
 # this process will be repeated six times
 # Eventually it should let the user choose which library is used for generation
 
-def toss
+library[[1,'a'][2,'b'][3,'c'][4,'d'][5,'e'][6,'f']] #test library 
+
+def toss #Generates dice roll 
   roll = ''
   while true
-    if roll.length >= 5
+    if roll.length >= 1 #Currently set to one die
       break
     end
     roll = roll + rand(1..6).to_s
@@ -16,3 +18,5 @@ def toss
 end
 toss
 puts roll
+
+library.each do 
