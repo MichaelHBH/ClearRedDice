195d1e54b875f900571a2b08b28c873711610757
Michael Hebblethwaite <mhhebble@gmail.com>
2018-02-18 16:41:16 +0900

    added basic look up pattern, modified .git files.

--- CRD.rb
+++ CRD.rb
@@ -4,14 +4,34 @@
 # this process will be repeated six times
 # Eventually it should let the user choose which library is used for generation
 
-def roll
+library = [[1,'a'],[2,'b'],[3,'c'],[4,'d'],[5,'e'],[6,'f']] #test library
+pword = '' #holds password
+part  = '' #holds part of the password
+count = 0  #counter for password generation loop
+
+def toss   #dice toss method
   x = ''
   while true
-    if x.length >= 5
+    if x.length >= 1
       break
     end
     x = x + rand(1..6).to_s
   end
   return x
 end
-puts roll
+
+while true #password generation loop
+  if count <= 4
+    roll = toss.to_i
+    library.each do |lookup|
+      if roll == lookup[0]
+         part = lookup[1]
+      end
+    end
+    pword = pword + ' ' + part
+    count +=1
+  else
+    break
+  end
+end
+puts pword
